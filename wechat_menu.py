53a8e0ad1557e75bb670c0a29b280761ac1b322c
Rody <jiahuilaw@outlook.com>
2023-10-12 13:00:53 +0800

    first

--- wechat_menu.py
+++ wechat_menu.py
@@ -1,54 +1,102 @@
 import requests
+import schedule
+import time
 import json
 
-def create_menu():
-    appid = "wxf873a70dfb7d4518" # 替换为你的AppID
-    appsecret = "2db6a473b0fe0e62be1512a74ec968f1"
-    url_accessToken = f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={appid}&secret={appsecret}"
-    response = requests.get(url_accessToken)
-    
-    if response.status_code == 200:
-        data = response.json()
-        access_token = data.get("access_token")
-        expires_in = data.get("expires_in")
-        print(access_token, expires_in)
+# 您的微信公众号的AppID和AppSecret
+app_id = "wx0f0ee60215bf86d3"
+app_secret = "f53ea8167a6a468076300f8af7066d7a"
+access_token = None
+
+def get_stable_access_token():
+    url = "https://api.weixin.qq.com/cgi-bin/token"
+    params = {
+        "grant_type": "client_credential",
+        "appid": app_id,
+        "secret": app_secret
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    if "access_token" in data:
+        return data["access_token"]
     else:
-        print("请求失败，状态码：", response.status_code)
-        return None, None
+        print("Failed to get stable access token:", data)
+        return None
     
-    url = f"https://api.weixin.qq.com/cgi-bin/menu/create?access_token={access_token}"
-    headers = {'Content-Type': 'application/json'}
-    data = {
+def create_custom_menu():
+    access_token = get_stable_access_token()
+    menu_data = {
         "button": [
             {
-                "type": "click",
-                "name": "菜单1",
-                "key": "menu1"
-            },
-            {
-                "name": "菜单2",
-                "sub_button": [
+                "name": "体检信息",
+                "sub_button":
+                [
                     {
                         "type": "view",
-                        "name": "子菜单1",
-                        "url": "http://example.com"
+                        "name": "查看体检报告",
+                        "url": "http://bg.kktijian.com/ReportSearch/Index?Login_YYBH=1070"
                     },
                     {
-                        "type": "click",
-                        "name": "子菜单2",
-                        "key": "submenu2"
+                        "type": "view",
+                        "name": "体检排队",
+                        "url" : "http://daojian.dtxd.com.cn/"
+
+                    }
+
+                ]
+            },
+            {
+                "name" : "预约挂号",
+                "sub_button":
+                [
+                    {
+                        "name": "小程序按钮",
+                        "type": "miniprogram",
+                        "url": "http://mp.weixin.qq.com",
+                        "appid": "wx0827f7b96e0f8948",
+                        "pagepath": "/pages/index/index"
+                    },
+                    {
+                        "type": "view",
+                        "name": "住院流程",
+                        "url" : "https://mp.weixin.qq.com/s/seZ5ENIx84ZoJUVP0EvTJQ"
+                    },
+                    {
+                        "type": "view",
+                        "name": "出院流程",
+                        "url" : "https://mp.weixin.qq.com/s/VRIbTm6y_8emlIwHrFDCUA"
                     }
                 ]
+            },
+            {
+                "type": "view",
+                "name": "北京专家",
+                "url": "https://mp.weixin.qq.com/s/taOrv2wiSNECSi5Yhskxmg"
             }
+
         ]
     }
-    json_data = json.dumps(data, ensure_ascii=False).encode('utf-8')
-    response = requests.post(url, data=json_data, headers=headers)
+    # 使用 Unicode 字符来表示特殊字符
+    menu_data_str = json.dumps(menu_data, ensure_ascii=False).encode('utf-8')
     
+    create_menu_url = f"https://api.weixin.qq.com/cgi-bin/menu/create?access_token={access_token}"
+    response = requests.post(create_menu_url, data=menu_data_str)
     if response.status_code == 200:
-        print("菜单创建成功")
+        print("自定义菜单创建成功！", response.text)
     else:
-        print("菜单创建失败")
+        print("自定义菜单创建失败：", response.text)
 
-if __name__ == '__main__':
-    create_menu()
+def update_menu_and_access_token():
+    get_stable_access_token()  # 更新 Access Token
+    create_custom_menu()  # 创建自定义菜单
+
+# 每半小时更新一次自定义菜单和 Access Token
+schedule.every(90).minutes.do(update_menu_and_access_token)
+
+if __name__ == "__main__":
+    get_stable_access_token()  # 获取初始 Access Token
+    create_custom_menu()  # 初始创建自定义菜单
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
